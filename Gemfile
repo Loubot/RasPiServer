48b06a416fc1b7abac6a614b427d64211689892e
Louis Angelini <lllouis@yahoo.com>
2014-03-02 21:12:31 +0000

    Allow cors

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ end
 
 module Temperature
   class Application < Rails::Application
+    #DISABLE CORS
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
